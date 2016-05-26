fd31b7090234f4bb2edb4ca877011f42c76937c4
kaos34k <kaos34@gmail.com>
2016-05-26 14:58:53 -0500

    nuevos métodos

--- models/datosModel.php
+++ models/datosModel.php
@@ -6,7 +6,7 @@ class datosModel {
 	private $nombre;
 	private $apellido;
 
-	public function __construct (){
+	public function __construct() {
 		$this->con = new Connect();
 	}
 
@@ -16,18 +16,34 @@ class datosModel {
 		return $data;
 	}
 
-	public function findByPropertyDatos( $property, $value ) {
+	public function findById( $id ) {
+		//$sql = "SELECT * FROM datos WHERE  =  ";
+		$sql= "SELECT * FROM datos WHERE 	id = '{$id}'";
+		$data = $this->con->runQuery($sql);
+		return $data;
+	}
+
+	public function findByProperty( $property, $value ) {
 		//$sql = "SELECT * FROM datos WHERE  =  ";
 		$sql= "SELECT * FROM datos WHERE 	{$property} = '{$value}'";
 		$data = $this->con->runQuery($sql);
 		return $data;
 	}
 
-	public function insert(){
+	public function insert() {
 		$sql = "INSERT INTO datos(nombre, apellido) 
 				VALUES ( {$this->nombre}, {$this->apellido} )";
 		$data = $this->con->runQuerySample($sql);
 
+	}
+	/*
+	* Estya funcion eliminara una lista de datos segun los parametros que sean enviados
+	*/
+	public function deleteDatos( $datas = array() ) {
+		foreach ( $datas as $key => $value) {
+			$sql = "DELETE FROM datos WHERE {$key} = '{$value}'";
+			$data = $this->con->runQuerySample($sql);		
+		}
 	}
 
 
@@ -36,10 +52,10 @@ class datosModel {
 
 $datos = new datosModel();
 $result = $datos->listDatas();
-$resultP = $datos->findByPropertyDatos( 'nombre','yesion' );
+$resultP = $datos->findByProperty( 'nombre','yesion' );
 
 while ( $row =  mysqli_fetch_array( $result ) ) {
-	echo $row['nombre']."-- ". $row['apellido'] ."</br>"; 
+	echo $row['nombre']." -- ". $row['apellido'] ."</br>"; 
 }
 
 while ( $row =  mysqli_fetch_array( $resultP ) ) {
